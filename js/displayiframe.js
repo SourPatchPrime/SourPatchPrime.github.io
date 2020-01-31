cc6c41a467442a72cf9c19c9d7d25911b3197255
SourPatchPrime <19481420+SourPatchPrime@users.noreply.github.com>
2020-01-30 21:53:39 -0800

    Main Page
    Created main page to contain content, implemented JS functions for displaying content

---
+++ js/displayiframe.js
@@ -0,0 +1,11 @@
+function displayFizzBuzz() {
+	document.getElementById("content").innerHTML = "<iframe src='fizzbuzz.html' style='height: 100%; width: 100%; border: 1px solid #ccc6b0;' ></iframe>";
+}
+
+function displayPlaceholder01() {
+	document.getElementById("content").innerHTML = "<p>Placeholder01</p>";
+}
+
+function displayPlaceholder02() {
+	document.getElementById("content").innerHTML = "<p>Placeholder02</p>";
+}
