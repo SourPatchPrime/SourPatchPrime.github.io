a75411c351f91384932e6e6550a9880e694590df
Dillon Johnson <19481420+SourPatchPrime@users.noreply.github.com>
2020-02-05 19:57:40 -0800

    Styling Changes + Content Additions
    Standardizing iframe styles
    Updating with new content and reflecting such on homepage/buttons

--- js/displayiframe.js
+++ js/displayiframe.js
@@ -2,10 +2,10 @@ function displayFizzBuzz() {
 	document.getElementById("content").innerHTML = "<iframe src='./content/fizzbuzz.html' style='height: 100%; width: 100%; border: 1px solid #ccc6b0;' ></iframe>";
 }
 
-function displayPlaceholder01() {
-	document.getElementById("content").innerHTML = "<p>Placeholder01</p>";
+function displayAbout() {
+	document.getElementById("content").innerHTML = "<iframe src='./content/about.html' style='height: 100%; width: 100%; border: 1px solid #ccc6b0;' ></iframe>";
 }
 
 function displayPlaceholder02() {
-	document.getElementById("content").innerHTML = "<p>Placeholder02</p>";
+	document.getElementById("content").innerHTML = "<p class='text'>Placeholder02</p>";
 }
